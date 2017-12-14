fd8dd4d5cd125d76ebb04e68ed91805b4db93016
MADONOKOUKI <madonomadonorunning@gmail.com>
2017-12-14 20:36:45 +0900

    URLから各サイトに飛ぶことが可能になった

--- naive_Bayes/app/views.py
+++ naive_Bayes/app/views.py
@@ -2,9 +2,26 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http.response import HttpResponse
-
+from datetime import datetime
 def hello_template(request):
-    return render(request, 'index.html')
+    d = {
+        'hour': datetime.now().hour,
+        'message': 'Sample message',
+    }
+    return render(request, 'index.html', d)
 
 def hello_world(request):
     return HttpResponse('Hello World!')
+
+def hello_if(request):
+    d = {
+        'is_visible': False,
+        'empty_str': '',
+    }
+    return render(request, 'if.html', d)
+
+def hello_for(request):
+    d = {
+        'objects': range(10),
+    }
+    return render(request, 'for.html', d)
