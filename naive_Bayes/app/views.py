5b1532827c645632ae69668e18a05c01c292d4d3
MADONOKOUKI <madonomadonorunning@gmail.com>
2017-12-14 17:52:22 +0900

    templateからURLを引っ張ってくることが出来るようになった

--- naive_Bayes/app/views.py
+++ naive_Bayes/app/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 from django.http.response import HttpResponse
 
+def hello_template(request):
+    return render(request, 'index.html')
 
 def hello_world(request):
     return HttpResponse('Hello World!')
