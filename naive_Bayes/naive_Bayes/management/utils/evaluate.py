fba6224162bc4259e1ed169233c8d36afdf64639
MADONOKOUKI <madonomadonorunning@gmail.com>
2017-12-21 00:41:57 +0900

    精度0.9362314385,過学習気味の方が、名詞と動詞と形容詞の重みが強く出るのでよい

--- naive_Bayes/naive_Bayes/management/utils/evaluate.py
+++ naive_Bayes/naive_Bayes/management/utils/evaluate.py
@@ -4,10 +4,11 @@ def train_and_evaluate_model(nb,x_train,y_train,train,test):
       cnt = 0
       for i in train:
         nb.train(x_train[i],y_train[i])
+      for i in train:
+        nb.train(x_train[i],y_train[i])
+      for i in train:
+        nb.train(x_train[i],y_train[i])
       for i in test:
         if(nb.classify(x_train[i]) == y_train[i]):
           cnt+=1;
-          print(nb.classify(x_train[i]))
-          print(y_train[i])
-
       return cnt / len(test)
